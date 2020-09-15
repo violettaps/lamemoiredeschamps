4941632e39b2b3e470cfc78405e6fc90fdc54aa1
Mathieu Pouëdras <7620812+mathieupouedras@users.noreply.github.com>
2020-09-15 12:26:11 +0200

    Filtre sur les saisons ok

--- js/controllers.js
+++ js/controllers.js
@@ -5,15 +5,52 @@ paintingsControllers.controller('PaintingsCtrl', function ($scope, $http, $sce, 
 });
 
 paintingsControllers.controller('AllPaintingsCtrl', function ($scope, $http, $sce, $location) {
+  $scope.isPrintemps = false
+  $scope.isEte = false
+  $scope.isAutomne = false
+  $scope.isHiver = false
+  $scope.filtrer = function() {
+    $scope.paintings = $scope.dataPaintings.filter(function(element) {
+      if (element.saison) {
+          if ($scope.isPrintemps && !element.saison.includes("printemps")) {
+            return false
+          }
+          if ($scope.isEte && !element.saison.includes("ete")) {
+            return false
+          }
+          if ($scope.isAutomne && !element.saison.includes("automne")) {
+            return false
+          }
+          else if ($scope.isHiver && !element.saison.includes("hiver")) {
+            return false
+          }
+          return true
+
+      } else {
+        return false
+      }
+    })
+  }  
+
+  $scope.supprimerFiltres = function() {
+    $scope.isPrintemps = false
+    $scope.isEte = false
+    $scope.isAutomne = false
+    $scope.isHiver = false
+    $scope.paintings = $scope.dataPaintings
+  }
+
   loadData($scope, $http, $sce, $location, false);
 });
 
 function loadData($scope, $http, $sce, $location, isPrivateHidden) {
   $scope.paintings = [];
+  $scope.dataPaintings = []
   $scope.location = $location.path();
 
   $http.get('data/data.json').success(function(data) {
     $scope.paintings = data;
+    $scope.dataPaintings = data
     if (isPrivateHidden) {
       hidePrivatePaintings($scope);
     }
@@ -46,4 +83,3 @@ function partition(arr, size) {
   }
   return newArr;
 }
-
